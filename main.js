59e946375e638e2678d27db8c0ccfa4ed955b661
Samuellekpor <samuellekpor@gmail.com>
2022-09-08 09:26:23 +0000

    Create js work section objects

--- main.js
+++ main.js
@@ -13,3 +13,34 @@ window.addEventListener('scroll', () => {
   menu.classList.remove('fa-times');
   navigation.classList.remove('nav-toggle');
 });
+
+const cards = [
+  {
+    image: 'images/snapshot5.png',
+    title: 'Tonic',
+    datas: ['CANOPY', 'Back End Dev', '2015'],
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    skills: ['html', 'css', 'javascript'],
+  },
+  {
+    image: 'images/snapshot4.png',
+    title: 'Multi-Post Stories',
+    datas: ['FACEBOOK', 'Full Stack Dev', '2015'],
+    description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    skills: ['html', 'Ruby on rails', 'css', 'javascript'],
+  },
+  {
+    image: 'images/snapshot1.png',
+    title: 'Facebook 360',
+    datas: ['FACEBOOK', 'Full Stack Dev', '2015'],
+    description: 'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+    skills: ['html', 'Ruby on rails', 'css', 'javascript'],
+  },
+  {
+    image: 'images/snapshot2.png',
+    title: 'Uber Navigation',
+    datas: ['Uber', 'Lead Developer', '2018'],
+    description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    skills: ['html', 'Ruby on rails', 'css', 'javascript'],
+  },
+];
